999d679e7c5ec1795171c3949ef1c7b4adc89a41
davidestebancamacho <davidestebancamacho@gmail.com>
2026-06-04 15:16:45 -0500

    cambio

--- src/app_gradio.py
+++ src/app_gradio.py
@@ -441,7 +441,7 @@ if __name__ == "__main__":
     demo.launch(
         server_name="127.0.0.1",
         server_port=7861,
-        share=False,
+        share=True,
         show_error=True,
         inbrowser=True
     )
